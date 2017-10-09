5e7ea87b09a7b8acb27a71214611e76d6bad4252
zcapwhl <zcapwhl@CT00006957.ad.ucl.ac.uk>
2017-10-09 16:53:07 +0100

    1.0

--- PHAS3459/src/module2/ThreeVector.java
+++ PHAS3459/src/module2/ThreeVector.java
@@ -14,13 +14,13 @@ public class ThreeVector {
 		ThreeVector uv = new ThreeVector(1.0, 2.0, 3.0);
 		uv = new ThreeVector(this.x/magnitude(), this.y/magnitude(), this.z/magnitude());
 		return uv;  
-
 	}
-
 	
 	public ThreeVector() {};
 	public ThreeVector(double a, double b, double c) {x=a; y=b; z=c;}
-
+	
 	public static void main (String args[]){
+	
+	ThreeVector v1 = new 
 	}
 }
